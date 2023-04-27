fc0d36261d01e22b2d3f7d611258aed53ddd7857
hassan0424 <hassan@shoplive.cloud>
2023-04-27 20:03:02 +0900

    dkdkdk

--- Package.swift
+++ Package.swift
@@ -7,12 +7,10 @@ let package = Package(
     name: "ShopliveStudioSDK",
     platforms: [.iOS(.v11)],
     products: [
-        .library(name: "ShopliveStudioSDK", targets: ["ShopliveStudioSDKTarget"]),
+        .library(name: "ShopliveStudioSDK",type: .static,  targets: ["ShopliveStudioSDKTarget"]),
     ],
     dependencies: [
-        .package(name: "PanModal", url: "https://github.com/slackhq/PanModal.git", .upToNextMajor(from: "1.0.0")),
-        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
-        .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.0"))
+        
     ],
     targets: [
         .binaryTarget(
@@ -22,10 +20,7 @@ let package = Package(
         .target(
             name: "ShopliveStudioSDKTarget",
             dependencies: [
-                .target(name: "ShopliveStudioSDK"),
-                .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "PanModal", package: "PanModal"),
-                .product(name: "SnapKit", package: "SnapKit")
+                .target(name: "ShopliveStudioSDK")
             ]
         )
     ]
